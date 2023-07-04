e92703854ec8b85ca8b39d4671244e8861fbd81e
Landon Thull <landonthull.tx@gmail.com>
2023-07-04 18:20:55 -0500

    style: add shadow to chips

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ function App() {
       <div className="w-full flex flex-col text-center justify-center content-middle md:w-1/2">
         <h1 className="text-5xl font-light text-timber md:text-7xl">Landon Thull</h1>
         <div className="mt-4 flex gap-4 w-full flex-wrap justify-center">
-          <h3 className="inline whitespace-nowrap px-4 py-2 bg-black rounded-lg text-blue">Software Engineer</h3>
-          <h3 className="inline whitespace-nowrap px-4 py-2 bg-black rounded-lg text-blue">CS Student</h3>
-          <h3 className="inline whitespace-nowrap px-4 py-2 bg-black rounded-lg text-blue">Coding Enthusiast</h3>
+          <h3 className="inline whitespace-nowrap px-4 py-2 bg-black rounded-lg text-blue shadow-md">Software Engineer</h3>
+          <h3 className="inline whitespace-nowrap px-4 py-2 bg-black rounded-lg text-blue shadow-md">CS Student</h3>
+          <h3 className="inline whitespace-nowrap px-4 py-2 bg-black rounded-lg text-blue shadow-md">Coding Enthusiast</h3>
         </div>
       </div>
       <h2 className="mt-32 text-3xl text-timber font-extralight">Website Coming Soon</h2>
