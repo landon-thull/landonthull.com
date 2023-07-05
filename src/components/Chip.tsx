ff340a21a6b1c74452c2b3d750f7865c0c5a7707
Landon Thull <landonthull.tx@gmail.com>
2023-07-04 21:15:23 -0500

    feat: add Chip component
    The chip component generates the chips under the main heading

---
+++ src/components/Chip.tsx
@@ -0,0 +1,15 @@
+
+
+interface IProps {
+  title: string
+}
+
+const Chip: React.FC<IProps> = ({ title }) => {
+  return (
+    <p className="inline whitespace-nowrap px-4 py-2 bg-black rounded-lg text-blue shadow-md">
+      {title}
+    </p>
+  )
+}
+
+export default Chip;
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,11 +1,19 @@
+import Chip from "./Chip";
+
+const chipTitles: string[] = [
+  "Software Engineer", 
+  "CS Student", 
+  "Coding Enthusiast"
+]
+
 const HeroSection: React.FC = () => {
   return (
     <div className="w-full flex flex-col text-center justify-center content-middle md:w-1/2">
       <h1 className="text-5xl font-light text-timber md:text-7xl">Landon Thull</h1>
       <div className="mt-4 flex gap-4 w-full flex-wrap justify-center">
-        <h3 className="inline whitespace-nowrap px-4 py-2 bg-black rounded-lg text-blue shadow-md">Software Engineer</h3>
-        <h3 className="inline whitespace-nowrap px-4 py-2 bg-black rounded-lg text-blue shadow-md">CS Student</h3>
-        <h3 className="inline whitespace-nowrap px-4 py-2 bg-black rounded-lg text-blue shadow-md">Coding Enthusiast</h3>
+        {chipTitles.map((chipTitle) => (
+          <Chip key={chipTitle} title={chipTitle} />
+        ))}
       </div>
       <h2 className="mt-32 text-3xl text-timber font-extralight">Website Coming Soon</h2>
     </div>
