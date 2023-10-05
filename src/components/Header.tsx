b10267e967c8c641b396ab5c365482d9975c99d9
Landon Thull <landonthull.tx@gmail.com>
2023-10-04 22:43:31 -0500

    style: adjust header positioning
    make header fixed instead of sticky. Allows for content to be properly centered.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import Link from "next/link";
+import {useState} from "react";
+
 type NavLink = {
   label: string;
   link: string;
@@ -11,27 +14,28 @@ const navLinks: NavLink[] = [
     link: "/",
   },
   {
+    // not implemented yet
     label: "Projects",
-    link: "/projects"
+    link: "#"
   }
 ];
 
-const navItems = navLinks.map((link) => (
-    <a
-        key={link.label}
-        href={link.link}
-        className="text-timber font-normal"
-        onClick={(event) => {
-          event.preventDefault();
-        }}
-    >
-      {link.label}
-    </a>
-));
-
 const Header = () => {
+  const [active, setActive] = useState<string>(navLinks[0].link);
+
+  const navItems = navLinks.map((link) => (
+      <Link
+          href={link.link}
+          key={link.link}
+          onClick={() => setActive(link.link)}
+          className={`${active === link.link ? "text-blue font-semibold" : "text-timber"} text-lg`}
+      >
+        {link.label}
+      </Link>
+  ));
+
   return (
-      <header className="sticky top-0 h-16 px-4 bg-black flex justify-center">
+      <header className="fixed w-screen top-0 z-50 h-20 px-4 bg-black flex justify-center">
         <div className="max-w-7xl grow flex justify-between items-center">
           <p className="text-timber text-2xl font-bold">Landon Thull</p>
           <nav className="flex items-center gap-x-4">
