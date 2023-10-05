1583ba430380ba1ebbee821838276ac584b2ce1d
Landon Thull <landonthull.tx@gmail.com>
2023-10-04 22:06:10 -0500

    feat: add Header component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Header from "@/components/Header";
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +14,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="bg-fixed bg-gradient-to-b from-black from-10% to-gunmetal">
+          <Header/>
+          {children}
+      </body>
     </html>
   )
 }
