7187b751a7ae0da35a98f7a23fbe941a566eebfe
Landon Thull <landonthull.tx@gmail.com>
2023-10-04 21:15:32 -0500

    feat: add metadata to root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import Image from 'next/image'
-import RootLayout from "@/app/layout";
+import {Metadata} from "next";
 
-export default function Home() {
-  return (
-      <RootLayout>
-        <h1>Hello World!</h1>
-      </RootLayout>
-  )
+export const metadata: Metadata = {
+  title: "Landon Thull | Software Engineer",
+  description: "I'm Landon Thull, a computer science student and passionate software engineer. " +
+      "Here, I showcase my personal projects and other related content."
 }
+
+const Page = () => {
+  return (
+      <h1>Hello World!</h1>
+  );
+}
+
+export default Page;
