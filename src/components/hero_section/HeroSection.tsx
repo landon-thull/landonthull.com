88e8f60bfcde0fbd9222eba201f7baabd8323eda
Landon Thull <landonthull.tx@gmail.com>
2023-10-05 03:52:54 -0500

    style: make chips mono font

--- src/components/hero_section/HeroSection.tsx
+++ src/components/hero_section/HeroSection.tsx
@@ -10,7 +10,7 @@ const HeroSection = () => {
   return (
       <div className="flex justify-center items-center flex-col h-screen gap-y-8">
         <h1 className="text-6xl font-light text-timber">Landon Thull</h1>
-        <div className="flex gap-4 w-full flex-wrap justify-center">
+        <div className="flex gap-4 w-full flex-wrap justify-center font-mono">
           {chipTitles.map((chipTitle) => (
               <HeroSectionChip label={chipTitle} key={chipTitle}/>
           ))}
