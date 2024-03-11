dd6d58cebefbbd5c2df0e54d90f80ea97ddfb825
Landon Thull <landonthull.tx@gmail.com>
2024-03-11 14:53:51 -0700

    modify header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,8 +7,8 @@ export default function Header() {
   const [active, setActive] = useState<string | null>(null);
 
   return (
-    <div className="w-screen justify-center flex">
-      <div className="fixed justify-start w-screen md:w-auto bg-opacity-75 hover:bg-opacity-100 transition-all pt-4 h-16 border-l border-r border-b border-neutral-600 z-50 px-8 py-4 rounded-b-lg bg-neutral-950 text-neutral-200">
+    <div className="w-screen fixed z-50 justify-center flex">
+      <div className="fixed justify-start w-screen pt-4 h-16 border-b opacity-75 border-neutral-600 z-50 px-8 py-4 bg-neutral-950 text-neutral-200">
         <nav className="h-full flex items-center justify-center gap-x-8 font-light">
           <Link href="/" className="hover:text-lg transition-all">
             Home
